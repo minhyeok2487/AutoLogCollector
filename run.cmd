3aa4a4a491a51930e8c7b145649f99c31a4587ec
Minhyeok1995 <repeater2487@naver.com>
2026-01-26 19:46:40 +0900

    Move configuration files to config folder

--- run.cmd
+++ run.cmd
@@ -10,9 +10,9 @@ rem Create log folder
 if not exist "logs\%today%" mkdir "logs\%today%"
 
 rem Execute commands for each server
-for /f "tokens=1,2 delims=," %%a in (servers.csv) do (
+for /f "tokens=1,2 delims=," %%a in (config\servers.csv) do (
     echo [%%a] %%b Processing...
-    (echo y & type commands.txt) | plink.exe %user%@%%a -pw %password% >"logs\%today%\%%b.log" 2>&1
+    (echo y & type config\commands.txt) | plink.exe %user%@%%a -pw %password% >"logs\%today%\%%b.log" 2>&1
     echo [%%a] %%b Done
 )
 echo All tasks completed
