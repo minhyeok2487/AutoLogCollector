934dcf5872202a2868aeaf999c902fb1bb155137
Minhyeok1995 <repeater2487@naver.com>
2026-01-27 13:34:10 +0900

    Remove unnecessary wails dev scripts

--- run_wails_dev.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set PATH=C:\Program Files\Go\bin;%USERPROFILE%\go\bin;%PATH%
-cd /d "%~dp0"
-wails dev
--- run_wails_dev.ps1
+++
@@ -1,3 +0,0 @@
-$env:PATH = "C:\Program Files\Go\bin;C:\Users\user\go\bin;" + $env:PATH
-Set-Location $PSScriptRoot
-& "C:\Users\user\go\bin\wails.exe" dev
